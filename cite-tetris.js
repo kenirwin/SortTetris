c7d316cfc0d01db2788a38620843251fccb251ad
Ken Irwin <kirwin@wittenberg.edu>
2016-04-03 20:06:17 -0400

    built some citations into js file

--- cite-tetris.js
+++ cite-tetris.js
@@ -1,7 +1,38 @@
+
+
 var game = {
     init: function () {
 //        alert ('test: init');
         var i;
+data = [
+    { 
+        "citation": "ATQ. Jun98, Vol. 12 Issue 2, p145.",
+        "type" : "journal article"
+    },
+    { 
+        "citation": "Burke, Susan L., et al. ''Letters.'' Ms 22.1 (2012): 6-8.", 
+        "type" : "journal article"
+    },
+    { 
+        "citation": "Albert H. University of Chicago Law Review. Spring2008, Vol. 75 Issue 2, p649-714.",
+        "type": "journal"
+    },
+    {
+        "citation": "The New Woman in Fiction and in Fact: Fin-de-Siecle Feminisms. 122-135. New York, NY: Palgrave, 2001.", 
+        "type": "book"
+    },
+    {
+        "citation": 'BRENNAN, PATRICK H. 2006. "Journalism." Oxford Companion To Canadian History 332-333.',
+        "type": "book chapter"
+    },
+    {
+        "citation": "Women of Minnesota : selected biographical essays. St. Paul : Minnesota Historical Society Press, 1977", 
+        "type": "book"
+    }
+];
+
+
+
         game.rows = [];
         game.activeRow = 0;
         game.citeText = '';
@@ -11,6 +42,7 @@ var game = {
             game.rows[i] = -1; //empty
         }
         game.bound = $.browser == 'msie' ? '#game' : window;
+        alert (data[1].type);
     },
 
     start: function () {
