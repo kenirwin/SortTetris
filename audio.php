4752fc997b93bfcb352c105e0a1f274cf435404a
Ken Irwin <kirwin@wittenberg.edu>
2016-07-05 14:46:59 -0400

    audio use optional

---
+++ audio.php
@@ -0,0 +1,31 @@
+    <audio id='myaudio' loop='loop'>
+      <source src='Korobeiniki.mp3' type='audio/mp3'>
+    </audio>
+
+    <script>
+   var audio=document.getElementById('myaudio');
+   audio.playbackRate = 1;
+   function playaudio()
+      {
+        audio.play()
+      }
+    function pauseaudio()
+      {
+       audio.pause()
+      }
+    function speedUp() 
+      {
+	var newRate = audio.playbackRate + 0.2;
+	if (newRate < 3) { 
+	  audio.playbackRate = newRate;
+	}
+      }
+    function slowDown() 
+      {
+	var newRate = audio.playbackRate - 0.2;
+	if (newRate < .5) { 
+	  audio.playbackRate = newRate;
+	}
+      }
+    </script>
+
