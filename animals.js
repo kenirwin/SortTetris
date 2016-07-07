79bb926ea23195a497bd700a6225c345520c1c46
Ken Irwin <kirwin@wittenberg.edu>
2016-07-07 12:59:19 -0400

    added alternate settings for non-bib usage

---
+++ animals.js
@@ -0,0 +1,62 @@
+data = [
+    { 
+        "item": "dog",
+        "type" : "mammal"
+    },
+    { 
+        "item": "cat",
+        "type" : "mammal"
+    },
+    { 
+        "item": "horse",
+        "type" : "mammal"
+    },
+    { 
+        "item": "sparrow",
+        "type" : "bird"
+    },
+    { 
+        "item": "robin",
+        "type" : "bird"
+    },
+    { 
+        "item": "stork",
+        "type" : "bird"
+    },
+    { 
+        "item": "bluegill",
+        "type" : "fish"
+    },
+    { 
+        "item": "salmon",
+        "type" : "fish"
+    },
+    { 
+        "item": "perch",
+        "type" : "fish"
+    },
+    { 
+        "item": "alligator",
+        "type" : "reptile"
+    },
+    { 
+        "item": "komodo dragon",
+        "type" : "reptile"
+    },
+    { 
+        "item": "iguana",
+        "type" : "reptile"
+    },
+    { 
+        "item": "frog",
+        "type" : "amphibian"
+    },
+    { 
+        "item": "salamander",
+        "type" : "amphibian"
+    },
+    { 
+        "item": "newt",
+        "type" : "amphibian"
+    },
+]
---
+++ settings_animal.php
@@ -0,0 +1,9 @@
+<?php
+$audioOK = false; // set to true to allow music
+$buttons = array ('mammal','bird','reptile','amphibian','fish');
+$data_file = 'animals.js';
+$item_label = 'animal';
+$game_title = 'Animal Tetris';
+$game_logo_file = ''; //leave blank for text title
+?>
+
