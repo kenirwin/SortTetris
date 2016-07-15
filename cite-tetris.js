2a6d9b8db0af18b96f5539d0de84061566e82718
Ken Irwin <kirwin@wittenberg.edu>
2016-07-15 09:43:55 -0400

    added shuffle fn, did not implement

--- cite-tetris.js
+++ cite-tetris.js
@@ -234,6 +234,18 @@ var game = {
         }
     },
     
+    shuffle: function(array) {
+        var currentIndex = array.length, temporaryValue, randomIndex;
+        while (0 !== currentIndex) {
+            randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex -= 1;
+            temporaryValue = array[currentIndex];
+            array[currentIndex] = array[randomIndex];
+            array[randomIndex] = temporaryValue;
+        }
+        return array;
+    }
+
     listProperties: function(obj) {
         var list='';
         for (var propertyName in obj) {
