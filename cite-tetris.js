kenirwin/SortTetris

commit a6153417442b9729f95076f3c17222ddca8aa5f2

prettied up gameover screen

cite-tetris.js
@@ -28,11 +28,13 @@ var game = {
             game.playerName = $('#name').val();
             game.submitScore();
             $('#name-entry').hide();
-            $('#name-display').text(game.playerName).show();
+            $('#close-gameover').removeClass('inactive');
+            $('#name-display').text('Player: ' + game.playerName).show();
         });
         $('#close-gameover').unbind().click(function() {
             $('#score').append('<br />Final Score with Accuracy Bonus: ' + game.finalScore);
             $('#gameover').hide();
+            $(this).addClass('inactive');
         });
 
     },

index.php
@@ -65,15 +65,18 @@ include('process_settings.php');
 </div>
 
 <form id="name-entry">
-<input type="text" id="name" size="10">
-<input type="button" id="name-submit" value="Enter">
+<input type="text" placeholder="your name"  id="name" size="10">
+<span id="name-submit" type="button" class="button">-></span>
+
 </form>
 
-<div id="name-display"></div>
+<h3 id="name-display"></h3>
 
+<h2>High Scores</h2>
+<center>
 <div id="leaderboard"></div>
-
-<center><div id="close-gameover" class="button">Close</div></center>
+<div id="close-gameover" class="button inactive">Close</div>
+</center>
 </div>
 
 <?php

style.css
@@ -30,7 +30,15 @@ td {
 #long-stats table { position: absolute }
 #debug { position: absolute; left: 1000px; display:none }
 #license { margin-top: 3em; }
-#gameover { position: absolute; top: 10%; left: 5%; width: 90%; height: 80%; z-index:100; background-color: #999; border: 10px solid gold; border-radius: 10px; display:none; font-family: Atari}
+#gameover { position: absolute; top: 10%; left: 5%; width: 90%; z-index:100; background-color: #999; border: 10px solid gold; border-radius: 10px; display:none; font-family: Atari; text-align: center;  line-height: 1.7em}
+
+#gameover input[type="text"] {
+    border: 5px solid #4CAF50;
+    background-color: #4CAF50;
+    font-family: Atari;
+    font-size: 1.5em; }
+}
+#gameover table td { padding: 0; }
 #close-gameover { }            
 
 .overlay {
@@ -59,7 +67,7 @@ button, .button {
     border-radius: 10px;
 }
 
-button.inactive {
+.inactive {
     background-color: lightgrey;
     color: darkgrey;
 }

commit 6b7ffda802ae0a5b154ef049b3f156b3fb9936e5

fixed long-stats percent-reporting bug introduced in a615341

cite-tetris.js
@@ -90,6 +90,7 @@ var game = {
         var answers = [];
         game.possibleAnswers = game.buttons;
         game.possibleAnswers.push('No Answer');
+        game.percent =  Math.round(100*game.nCorrect/game.nTotal);
         for (i = 0; i < game.answerLog.length; i++) {
             var correct=game.answerLog[i][0];
             var answered=game.answerLog[i][1];
@@ -142,8 +143,8 @@ var game = {
                         else {
                             var score_class = 'score-incorrect';
                         }
-                        game.percent =  Math.round(100*game.nCorrect/game.nTotal);
-                        output+= '<tr class="'+score_class+'"><td>' + key2 + '</td><td>' +answers[key1][key2]+ '</td><td>' + game.percent +'%</td></tr>';                        
+                        percent = Math.round(percent*100);
+                        output+= '<tr class="'+score_class+'"><td>' + key2 + '</td><td>' +answers[key1][key2]+ '</td><td>' + percent +'%</td></tr>';                        
                     }
                 }
             }