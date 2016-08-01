80b3a2f6afebffc8f988f0bd4653f1f3557c42a5
Ken Irwin <kirwin@wittenberg.edu>
2016-08-01 12:52:54 -0400

    updated prep_files to use simpler naming system; fail friendlier

--- prep_files.php
+++ prep_files.php
@@ -9,34 +9,47 @@ if (! isset($argv[1])) {
 else { 
     $realm = $argv[1];
     $input_files = GetInputs($realm);
-    $outfile = @fopen('data-files/'.$realm.'.json', 'w');
-    foreach ($input_files as $file) {
-        $handle = @fopen($maindir.'/'.$realm.'/'.$file, 'r');
-        $type = substr($file, 0, -1);
-        if ($handle) {
-            while (($buffer = fgets($handle, 4096)) !== false) {
-                $buffer = ltrim(chop($buffer));
-                $line['item']=strip_tags($buffer, '<em><i><img>');
-                $line['item']=preg_replace('/&nbsp;/',' ',$line['item']);
-                $line['type']=$type;
-                if (preg_match("/[a-zA-Z0-9]/",$line['item'])) { // if string has content
-                    array_push($contents, $line);
+    if ($input_files) { 
+        $total_files = 0;
+        foreach ($input_files as $file) {
+            $total_files++;
+            $input_path = $maindir.'/'.$realm.'/'.$file;
+            $handle = @fopen($input_path, 'r');
+            $type = $file;
+            if ($handle) {
+                while (($buffer = fgets($handle, 4096)) !== false) {
+                    $buffer = ltrim(chop($buffer));
+                    $line['item']=strip_tags($buffer, '<em><i><img>');
+                    $line['item']=preg_replace('/&nbsp;/',' ',$line['item']);
+                    $line['type']=$type;
+                    if (preg_match("/[a-zA-Z0-9]/",$line['item'])) { // if string has content
+                        array_push($contents, $line);
+                    }
                 }
+                if (!feof($handle)) {
+                    echo 'Error: unexpected fgets() fail'.PHP_EOL;
+                }
+                fclose($handle);
             }
-            if (!feof($handle)) {
-                echo 'Error: unexpected fgets() fail'.PHP_EOL;
-            }
-            fclose($handle);
         }
     }
+
+    
+
     if (sizeof($contents) < 1) {
         print 'No data available in '.$realm.PHP_EOL;
     }
-    elseif (fwrite($outfile, (json_encode($contents)))) {
-        print 'JSON data written to data-files/'.$realm.'.json'.PHP_EOL;
+    elseif ($total_files > 1) {
+        $outfile = @fopen('data-files/'.$realm.'.json', 'w');
+        if (fwrite($outfile, (json_encode($contents)))) {
+            print 'JSON data written to data-files/'.$realm.'.json'.PHP_EOL;
+        }
+        else {
+            print 'Could not write data to data-files/'.$realm.'.json'.PHP_EOL;
+        }
     }
     else {
-        print 'Could not write data to data-files/'.$realm.'.json'.PHP_EOL;
+        print 'Fewer than two input files in '.$maindir.'/'.$realm.'. At least two files are needed.'.PHP_EOL;
     }
 } //end else 
 
@@ -55,17 +68,23 @@ function DisplayFiles() {
 function GetInputs ($realm, $human_readable=false) {
     global $maindir;
     $output = array();
-    $files = scandir($maindir.'/'.$realm);
-    foreach ($files as $f) {
-        if (! (preg_match('/^\./', $f))) {
-            array_push($output, $f);
+    if (is_dir($maindir.'/'.$realm)) {
+            $files = scandir($maindir.'/'.$realm);
+            foreach ($files as $f) {
+                if (! (preg_match('/^\./', $f))) {
+                    array_push($output, $f);
+                }
+            }
+            $human_array = join (', ', $output);
+            if ($human_readable) { 
+                return $human_array;
+            }
+            else { return $output; } 
+        } //end if is_dir
+        else {
+            print 'Not a directory: '.$maindir.'/'.$realm.PHP_EOL;
+            return false;
         }
-    }
-    $human_array = join (', ', $output);
-    if ($human_readable) { 
-        return $human_array;
-    }
-    else { return $output; } 
 }
 
 ?>
