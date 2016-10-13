1f3b5fc7e50fd965e1d008892c5b451391dc6ae9
kenirwin <kirwin@wittenberg.edu>
2016-10-13 11:41:24 -0700

    start supervisor/reg form - reCaptcha authenticate

--- global_settings_example.php
+++ global_settings_example.php
@@ -8,6 +8,11 @@ $charset = "utf8";
 
 $audioOK = false; // set to true to allow music
 
+/* Captcha API keys */
+$using_captcha = false; //set to true when you add your keys (below)
+$captcha_site_key = "";
+$captcha_secret_key = ""; 
+
 /* Google Analytics ID */
 $google_analytics_id = ''; // usually something like UA-1234567-8
 ?>
---
+++ supervisor/register.php
@@ -0,0 +1,68 @@
+<html>
+<head>
+<title>Register as a Supervisor</title>
+<?php include("../global_settings.php"); ?>
+<script src='https://www.google.com/recaptcha/api.js'></script>
+</head>
+<body>
+<h2>Register as a Supervisor</h2>
+<?php
+   if (isset($_REQUEST['submit_button']) && (isset($_REQUEST['g-recaptcha-response']))) {
+     ConfirmHuman($captcha_secret_key);
+     SubmitSupervisorRequest();
+   }
+?>
+
+<form method="post">
+   <label for="name">Name</label><input type="text" name="name" /><br />
+   <label for="email">Email</label><input type="text" name="email" /><br />
+   <label for="inst_name">Institution Name</label><input type="text" name="inst_name" /><br />
+   <input type="submit" name="submit_button" value="Register" />
+   <div class="g-recaptcha" data-sitekey="<?php print($captcha_site_key); ?>"></div>
+</body>
+
+<?php
+   function ConfirmHuman($key) {
+	 $url = 'https://www.google.com/recaptcha/api/siteverify';
+	 $fields = array(
+			 'secret' => $key,
+			 'response' => $_REQUEST['g-recaptcha-response']
+			 
+			 );	 
+	 
+	 foreach($fields as $key=>$value) { $fields_string .= $key.'='.$value.'&'; }
+	 rtrim($fields_string, '&');
+	 
+	 //open connection
+	 $ch = curl_init();
+	 
+	 //set the url, number of POST vars, POST data
+	 curl_setopt($ch,CURLOPT_URL, $url);
+	 curl_setopt($ch,CURLOPT_POST, count($fields));
+	 curl_setopt($ch,CURLOPT_POSTFIELDS, $fields_string);
+	 curl_setopt($ch,CURLOPT_RETURNTRANSFER, true);
+	 
+	 //execute post
+	 $json = curl_exec($ch);
+	 
+	 //close connection
+	 curl_close($ch);
+	 $result = json_decode($json);
+	 if ($result->success == true) {
+	   return true;
+	 }
+	 else {
+	   print '<h3>Unable to process your request</h3>'.PHP_EOL;
+	   foreach ($result->{'error-codes'} as $error) {
+	     if ($error == 'missing-input-response') {
+	       print '<li class="warn">Be sure to check the "I&apos;m not a robot" box below</li>'.PHP_EOL;
+	     }
+	     else { print '<li class="warn">'.$error.'</li>'.PHP_EOL; }
+	   }
+	 }
+       }
+       
+function SubmitSupervisorRequest() {
+  
+}
+?>
