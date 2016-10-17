432159f2e3dbce95fe253ed55e3728f727588155
kenirwin <kirwin@wittenberg.edu>
2016-10-17 07:13:57 -0700

    use local jquery files

--- index.php
+++ index.php
@@ -7,12 +7,9 @@ session_start();
 <html>
 
 <head>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
-<link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/themes/smoothness/jquery-ui.css">
-<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
-<!--script type="text/javascript" src="./lib/jquery.js"></script-->
-<!--script type="text/javascript" src="./lib/jquery-ui.min.js"></script-->
-<!--link rel="stylesheet" type="text/css" href="lib/jquery-ui.css" /-->
+<script type="text/javascript" src="./lib/jquery.js"></script>
+<script type="text/javascript" src="./lib/jquery-ui.min.js"></script>
+<link rel="stylesheet" type="text/css" href="lib/jquery-ui.css">
 <link rel="stylesheet" type="text/css" href="style.css">
 
 <script>
