85aeef1e09174acc7d3ebca459396c85d874a21f
kenirwin <kirwin@wittenberg.edu>
2017-05-02 14:08:40 -0700

    starting to convert over to supervisor/ dir for all php-auth

--- supervisor/index.php
+++ supervisor/index.php
@@ -1,6 +1,6 @@
 <?php
 // Initialisation
-require_once('../includes/init.php');
+require_once('includes/init.php');
 // Require the user to be logged in before they can see this page.
 Auth::getInstance()->requireLogin();
 // Set the title, show the page header, then the rest of the HTML
