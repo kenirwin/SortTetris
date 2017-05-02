329f93553569b123041b2caa877258d09ecca4df
kenirwin <kirwin@wittenberg.edu>
2017-05-02 14:16:35 -0700

    slimmed down, still having re-direct trouble

--- supervisor/display_login.php
+++ supervisor/display_login.php
@@ -1,4 +1,4 @@
 <div id="logged-in">Logged in as: 
 <?php print($_SESSION['name'] .' @ '.$_SESSION['institution_name']); ?> 
-<a href="../logout.php" class="button-small">Logout</a>
+<a href="logout.php" class="button-small">Logout</a>
 </div>
