fa3661704d1b44ac229bb537d165be41a56cc7ac
kenirwin <kirwin@wittenberg.edu>
2017-05-02 13:53:50 -0700

    fixed typos in Config_Sample.class.php

--- supervisor/classes/Config_Sample.class.php
+++ supervisor/classes/Config_Sample.class.php
@@ -7,15 +7,16 @@
 class Config
 {
 
-  const DB_HOST = '',
-        DB_NAME = '',
-        DB_USER = '',
-        DB_PASS = '',
-        SMTP_HOST = '',
-        SMTP_USER = '',
-        SMTP_PASS = '',
-        SMTP_PORT = 587; // try 25 (unsecured) if that doesn't work
-        SMTP_SENDER = 'from@example.com';
-        REDIR_PATH = '';
-
+  const 
+    DB_HOST = '',
+    DB_NAME = '',
+    DB_USER = '',
+    DB_PASS = '',
+    SMTP_HOST = '',
+    SMTP_USER = '',
+    SMTP_PASS = '',
+    SMTP_PORT = 587, // try 25 (unsecured) if that doesn't work
+    SMTP_SENDER = 'from@example.com',
+    REDIR_PATH = ''; // http path to supervisor directory, no trailing slash
+  
 }
