77ed73bdd5c14e4b9cf08f9eae2490b89f187fdd
kenirwin <kirwin@wittenberg.edu>
2017-05-04 11:49:33 -0700

    new tables.sql

--- tables.sql
+++ tables.sql
@@ -1,51 +1,74 @@
--- phpMyAdmin SQL Dump
--- version 3.2.3
--- http://www.phpmyadmin.net
---
--- Host: localhost
--- Generation Time: Jul 28, 2016 at 11:57 AM
--- Server version: 5.6.30
--- PHP Version: 5.4.16
-
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
-
---
--- Database: `tetris`
---
-
-
--- --------------------------------------------------------
-
---
--- Table structure for table `institutions`
---
-
-CREATE TABLE IF NOT EXISTS `institutions` (
-  `institution_id` int(11) NOT NULL AUTO_INCREMENT,
-  `institution_name` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
-  `contact_email` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
-  `contact_name` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
-  `password` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
-  `activated` char(1) COLLATE utf8_unicode_ci NOT NULL DEFAULT 'N',
-  PRIMARY KEY (`institution_id`),
-  UNIQUE KEY `contact_email` (`contact_email`),
-  UNIQUE KEY `institution_name` (`institution_name`)
-) DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
-
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 --
 -- Table structure for table `leaderboard`
 --
-
-CREATE TABLE IF NOT EXISTS `leaderboard` (
-  `game_id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `leaderboard` (
+  `game_id` int(11) NOT NULL,
   `time_entry` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `username` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
   `score` int(11) NOT NULL,
   `percent` tinyint(4) NOT NULL,
   `level` tinyint(4) NOT NULL,
   `config_file` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
-  `inst_id` int(11) NOT NULL,
-  PRIMARY KEY (`game_id`)
-) DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+  `inst_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+--
+-- Table structure for table `remembered_logins`
+--
+CREATE TABLE `remembered_logins` (
+  `token` varchar(40) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `expires_at` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+--
+-- Table structure for table `users`
+--
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `institution_name` varchar(50) NOT NULL,
+  `name` varchar(128) NOT NULL,
+  `email` varchar(128) NOT NULL,
+  `password` varchar(60) NOT NULL,
+  `password_reset_token` varchar(40) DEFAULT NULL,
+  `password_reset_expires_at` datetime DEFAULT NULL,
+  `activation_token` varchar(40) DEFAULT NULL,
+  `is_active` tinyint(1) NOT NULL DEFAULT '0',
+  `is_admin` tinyint(1) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+--
+-- Indexes for table `leaderboard`
+--
+ALTER TABLE `leaderboard`
+  ADD PRIMARY KEY (`game_id`);
+--
+-- Indexes for table `remembered_logins`
+--
+ALTER TABLE `remembered_logins`
+  ADD PRIMARY KEY (`token`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `expires_at` (`expires_at`);
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`),
+  ADD UNIQUE KEY `institution_name` (`institution_name`),
+  ADD UNIQUE KEY `password_reset_token` (`password_reset_token`),
+  ADD UNIQUE KEY `activation_token` (`activation_token`),
+  ADD KEY `name` (`name`);
+--
+-- AUTO_INCREMENT for table `leaderboard`
+--
+ALTER TABLE `leaderboard`
+  MODIFY `game_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+--
+-- Constraints for table `remembered_logins`
+--
+ALTER TABLE `remembered_logins`
+  ADD CONSTRAINT `remembered_logins_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
