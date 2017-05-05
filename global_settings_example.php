31e1c24e006f4e8aeb2a4bcf7f0fa2e04175d49f
kenirwin <kirwin@wittenberg.edu>
2017-05-05 13:04:13 -0700

    added contact email back into global_settings

--- global_settings_example.php
+++ global_settings_example.php
@@ -12,7 +12,7 @@ $display_institution_select = false; // set to true to allow users to select the
 $display_supervisor_reg_links = true; //only effective if next setting==true
 $allow_supervisor_registration = false; 
 $allow_admin = false; //set to true once database has been set up in this file and in supervisor/classes/Config.class.php, only if you wish to allow supervisor logins
-
+$contact_email = ''; //email to which contact form submissions will be sent
 
 /* Captcha API keys */
 $using_captcha = false; //set to true when you add your keys (below)
