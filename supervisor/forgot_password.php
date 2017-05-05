d36607b04b7b764d8ed4220d4410a63708e4897b
kenirwin <kirwin@wittenberg.edu>
2017-05-05 14:14:47 -0700

    added captcha to forgot_password.php

--- supervisor/forgot_password.php
+++ supervisor/forgot_password.php
@@ -12,17 +12,28 @@ Auth::getInstance()->requireGuest();
 
 // Process the submitted form
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-  ob_start(); //added to handly mysterious output of SMTP code to the screen
-  Auth::getInstance()->sendPasswordReset($_POST['email']);
-  ob_end_clean(); //clean the buffer
-  $message_sent = true;
+  include('../global_settings.php');
+  $proceed = true;
+  if ($using_captcha) {
+    include('../captcha.php');
+    if (! ConfirmHuman($captcha_secret_key)) {
+      $proceed = false;
+      $message_sent = false;
+    }
+  }
+  if ($proceed === true) {
+    ob_start(); //added to handly mysterious output of SMTP code to the screen
+    Auth::getInstance()->sendPasswordReset($_POST['email']);
+    ob_end_clean(); //clean the buffer
+    $message_sent = true;
+  }
 }
 
 
 // Set the title, show the page header, then the rest of the HTML
 $page_title = 'Forgotten password';
 include('includes/header.php');
-
+include('../global_settings.php');
 ?>
 
 <h1>Forgotten password</h1>
@@ -31,6 +42,9 @@ include('includes/header.php');
   <p>If we found an account with that email address, we have sent password reset instructions to it. Please check your email.</p>
 
 <?php else: ?>
+     <?php if (isset($proceed) && $proceed === false): ?>
+	 <p class="warn">We could not verify your humanity. No email was sent.</p>
+     <?php endif ?>
 
   <form method="post" class="uk-form uk-form-horizontal">
     <div class="uk-form-row">
@@ -39,6 +53,14 @@ include('includes/header.php');
         <input id="email" name="email" type="email" required="required" autofocus="autofocus" />
       </div>
     </div>
+
+  <?php
+  if ($using_captcha) {
+    print '<div class="g-recaptcha" data-sitekey="'.$captcha_site_key.'"></div>';
+  }
+?>
+
+
 
     <div class="uk-form-row">
       <div class="uk-form-controls">
